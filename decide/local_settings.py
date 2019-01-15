c87e805a3bd53bc9df7242cc99a1eae10c8e3fc0
mrchavea <mrchavea>
2019-01-15 11:36:41 +0100

    Añadida configuracion para Travis

--- decide/local_settings.py
+++ decide/local_settings.py
@@ -2,15 +2,15 @@ ALLOWED_HOSTS = ["*"]
 
 # Modules in use, commented modules that you won't use
 MODULES = [
-    'authentication',
-    'base',
-    'booth',
-    'census',
+    # 'authentication',
+    # 'base',
+    # 'booth',
+    # 'census',
     'mixnet',
-    'postproc',
-    'store',
-    'visualizer',
-    'voting',
+    # 'postproc',
+    # 'store',
+    # 'visualizer',
+    # 'voting',
 ]
 
 APIS = {
